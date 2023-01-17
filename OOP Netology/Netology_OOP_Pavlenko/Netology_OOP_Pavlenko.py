3603edd0edfc311680bfb337b082967eeca33496
Maxim Pavlenko 77 <max.pavlenko@gmail.com>
2023-01-17 21:40:07 +0300

    Task2_completed

--- OOP Netology/Netology_OOP_Pavlenko/Netology_OOP_Pavlenko.py
+++ OOP Netology/Netology_OOP_Pavlenko/Netology_OOP_Pavlenko.py
@@ -7,6 +7,15 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Error'
+
         
 class Mentor:
     def __init__(self, name, surname):
@@ -14,6 +23,15 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
         
+    
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -22,22 +40,24 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Error'
-
-class Lecturer(Mentor):
-    pass
-
-
-class Reviewer(Mentor):
-    pass
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Physics']
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+
+
+
+best_lecturer = Lecturer('Albert', 'Einstein')
+best_lecturer.courses_attached += ['Physics']
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+best_student.rate_lecturer(best_lecturer, 'Physics', 9)
  
 print(best_student.grades)
+print(best_lecturer.grades)
